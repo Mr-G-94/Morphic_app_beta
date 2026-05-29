c9069ceb66f982a681c8ac98c8e3401bc71b15ba
Mr-G-94 <jorge.jrjv94@gmail.com>
2026-05-28 19:17:29 -0600

    Fix: Remove consumerProGuardFiles from all library modules (data, ui, di)

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -11,7 +11,6 @@ android {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProGuardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -23,42 +22,6 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation(project(":core"))
-    implementation(project(":data"))
-    implementation(project(":ui"))
-    // Dependencies for the shell to consume AgentRegistry and AgentContract
-    implementation(project(":di"))
-    implementation(project(":core-base"))
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.morphiclabs.di"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
